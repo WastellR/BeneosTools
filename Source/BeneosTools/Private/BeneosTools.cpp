4ad8972b17469901c5701ed61956d70d541185c2
Robbie <rwastell@34bigthings.com>
2025-01-21 11:29:18 +0200

    Add missing module .cpp file

---
+++ Source/BeneosTools/Private/BeneosTools.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) CORE Games, Ltd. All rights reserved.
+
+#include "BeneosTools.h"
+
+#define LOCTEXT_NAMESPACE "FBeneosToolsModule"
+
+void FBeneosToolsModule::StartupModule()
+{
+
+void FBeneosToolsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FBeneosToolsModule, BeneosTools)
