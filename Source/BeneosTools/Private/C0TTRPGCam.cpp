dd0ff8025d97405e9c7b9149de1b008f07346643
WastellR <robomooer@gmail.com>
2026-01-11 12:15:20 +0200

    I think mostly moving code from old class to new

--- Source/BeneosTools/Private/C0TTRPGCam.cpp
+++ Source/BeneosTools/Private/C0TTRPGCam.cpp
@@ -3,9 +3,8 @@
 #include "C0TTRPGCam.h"
 
 #include <Camera/CameraComponent.h>
-
 #include <Kismet/KismetMathLibrary.h>
-#include "C0CamGrid.h"
+#include "C0Grid.h"
 
 #pragma region Utility
 
@@ -39,43 +38,124 @@ FVector2D ProjectPointOntoLine(const FVector2D& Point, const FVector2D& LinePoin
 
 #pragma endregion Utility
 
-AC0TTRPGCam::AC0TTRPGCam() :    
+AC0TTRPGCam::AC0TTRPGCam() :
+    ParentGrid(nullptr),
+    AdjustmentMode(EC0AdjustmentMode::Automatic),
+    AspectRatio(0.f),
+    FOV(0.f),
+    TopOverlayMargin(0.f),
+    BottomOverlayMargin(0.f),
+    RightOverlayMargin(0.f),
+    LeftOverlayMargin(0.f),
     GridMarginInTiles(0.f),
-    Length(10),
-    Width(10),
-    TileSizeInCm(152.4)
+    bDebugDrawLineToFocalPoint(false),
+    bShowPIP(true),
+    bInitialized(false)
 {
     SetActorRotation(FRotator(-90.f, 0.f, 0.f));
+
+    // Create margin meshes
+    const FString MaterialPath = TEXT("/BeneosTools/M_MarginMaterial.M_MarginMaterial");
+    UMaterial* Material = Cast<UMaterial>(StaticLoadObject(UMaterialInterface::StaticClass(), nullptr, *MaterialPath));
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneMesh(TEXT("/Engine/BasicShapes/Plane.Plane"));
+    TopMarginComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TopMarginComponent"));
+    TopMarginComponent->SetupAttachment(RootComponent);
+    TopMarginComponent->SetVisibility(false);
+    TopMarginComponent->SetMaterial(0, Material);
+    TopMarginComponent->SetStaticMesh(PlaneMesh.Object);
+    TopMarginComponent->SetUsingAbsoluteRotation(false);
+    TopMarginComponent->SetCastShadow(false);
+    TopMarginComponent->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+    TopMarginComponent->SetRenderCustomDepth(false);
+    TopMarginComponent->bSelectable = false;
+    BottomMarginComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BottomMarginComponent"));
+    BottomMarginComponent->SetupAttachment(RootComponent);
+    BottomMarginComponent->SetVisibility(false);
+    BottomMarginComponent->SetMaterial(0, Material);
+    BottomMarginComponent->SetStaticMesh(PlaneMesh.Object);
+    BottomMarginComponent->SetUsingAbsoluteRotation(false);
+    BottomMarginComponent->SetCastShadow(false);
+    BottomMarginComponent->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+    BottomMarginComponent->SetRenderCustomDepth(false);
+    BottomMarginComponent->bSelectable = false;
+    RightMarginComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightMarginComponent"));
+    RightMarginComponent->SetupAttachment(RootComponent);
+    RightMarginComponent->SetVisibility(false);
+    RightMarginComponent->SetMaterial(0, Material);
+    RightMarginComponent->SetStaticMesh(PlaneMesh.Object);
+    RightMarginComponent->SetUsingAbsoluteRotation(false);
+    RightMarginComponent->SetCastShadow(false);
+    RightMarginComponent->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+    RightMarginComponent->SetRenderCustomDepth(false);
+    RightMarginComponent->bSelectable = false;
+    LeftMarginComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftMarginComponent"));
+    LeftMarginComponent->SetupAttachment(RootComponent);
+    LeftMarginComponent->SetVisibility(false);
+    LeftMarginComponent->SetMaterial(0, Material);
+    LeftMarginComponent->SetStaticMesh(PlaneMesh.Object);
+    LeftMarginComponent->SetUsingAbsoluteRotation(false);
+    LeftMarginComponent->SetCastShadow(false);
+    LeftMarginComponent->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+    LeftMarginComponent->SetRenderCustomDepth(false);
+    LeftMarginComponent->bSelectable = false;
 }
 
 void AC0TTRPGCam::OnConstruction(const FTransform& Transform)
 {
+    // Call initialization only the first time the actor is created in editor
+    if (!bInitialized)
+    {
+        bInitialized = true;
+        PrevAdjustmentMode = AdjustmentMode;
+        PrevParentGrid = ParentGrid;
+        if (ParentGrid)
+        {
+            ParentGrid->ChildCameras.Add(this);
+        }
+    }
+
     Super::OnConstruction(Transform);
+
+    HandleDebugDraw();
     
     UpdateCamera();
 }
 
-void AC0TTRPGCam::PostEditComponentMove(bool bFinished)
+void AC0TTRPGCam::PostEditMove(bool bFinished)
 {
-    if (Parent)
-    {
-        FVector PointToLine = GetActorLocation() - Parent->GetActorLocation();
-        FVector Dir = PointToLine;
-        Dir.Normalize();
-        if (Dir != Parent->GetActorUpVector())
-        {
-            float ProjectionLength = FVector::DotProduct(PointToLine, Parent->GetActorUpVector());
-            FVector ProjectionVector = Parent->GetActorUpVector() * ProjectionLength;
-            FVector ProjectedPoint = Parent->GetActorLocation() + ProjectionVector;
-            SetActorLocation(ProjectedPoint);
-            if (GEditor)
-            {
-                GEditor->SetPivot(ProjectedPoint, false, true, true);
-            }
-        }
-    }
+    Super::PostEditMove(bFinished);
 
     UpdateCamera();
+}
+
+void AC0TTRPGCam::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+    Super::PostEditChangeProperty(PropertyChangedEvent);
+    
+    if (PropertyChangedEvent.Property)
+    {
+        // If the ShowPIP property gets changed, re-select this actor 
+        // so that the change gets applied immediately
+        if (PropertyChangedEvent.Property->GetName() == "bShowPIP")
+        {
+            if (GEditor)
+            {
+                GEditor->SelectActor(this, true, true);
+            }
+        }
+        if (PropertyChangedEvent.Property->GetName() == "ParentGrid")
+        {
+            if (PrevParentGrid)
+            {
+                PrevParentGrid->ChildCameras.Remove(this);
+            }
+            if (ParentGrid)
+            {
+                ParentGrid->ChildCameras.Add(this);
+            }
+            PrevParentGrid = ParentGrid;
+        }
+    }
 }
 
 FString AC0TTRPGCam::GetDefaultActorLabel() const
@@ -83,78 +163,199 @@ FString AC0TTRPGCam::GetDefaultActorLabel() const
     return TEXT("Beneos Cam");
 }
 
-void AC0TTRPGCam::SetParent(AC0CamGrid* InParent)
-{
-    Parent = InParent;
-    Length = Parent->Length;
-    Width = Parent->Width;
-    TileSizeInCm = Parent->TileSizeInCm;   
-    GridMarginInTiles = Parent->GridMarginInTiles;
-    UpdateCamera();
-}
-
 void AC0TTRPGCam::UpdateCamera()
 {
-    if (!Parent)
+    if (!ParentGrid)
         return;
 
-    const float Margin = GridMarginInTiles * 2.f;
-    const float AutoAspectRatio = (Width + Margin) / (Length + Margin);
-    const float CamDist = FVector::Dist(Parent->GetActorLocation(), GetActorLocation());
-    const float HalfGridWidth = (Width + Margin) * TileSizeInCm / 2.f;
-    const float AutoFOV = FMath::RadiansToDegrees(2 * FMath::Atan(HalfGridWidth / CamDist));
-    // Adjust FOV + aspect ratio based on mode
-    if (Parent->AdjustmentMode != Parent->PrevAdjustmentMode)
+    // Constrain camera to ParentGrid grid's z-axis
+    FVector PointToLine = GetActorLocation() - ParentGrid->GetActorLocation();
+    FVector Dir = PointToLine;
+    Dir.Normalize();
+    if (Dir != ParentGrid->GetActorUpVector())
     {
-        if (Parent->AdjustmentMode == EC0AdjustmentMode::Automatic)
+        float ProjectionLength = FVector::DotProduct(PointToLine, ParentGrid->GetActorUpVector());
+        FVector ProjectionVector = ParentGrid->GetActorUpVector() * ProjectionLength;
+        FVector ProjectedPoint = ParentGrid->GetActorLocation() + ProjectionVector;
+        SetActorLocation(ProjectedPoint);
+        if (GEditor)
         {
-            Parent->AspectRatio = 0.f;
-            Parent->FOV = 0.f;
+            GEditor->SetPivot(ProjectedPoint, false, true, true);
         }
-        else if (Parent->AdjustmentMode == EC0AdjustmentMode::Additional &&
-            Parent->PrevAdjustmentMode == EC0AdjustmentMode::Manual)
-        {            
-            Parent->AspectRatio -= AutoAspectRatio;
-            Parent->FOV -= AutoFOV;
-        }
-        else if (Parent->AdjustmentMode == EC0AdjustmentMode::Manual)
-        {
-            if (Parent->PrevAdjustmentMode == EC0AdjustmentMode::Automatic)
-            {
-                Parent->AspectRatio = AutoAspectRatio;
-                Parent->FOV = AutoFOV;
-            }
-            else if (Parent->PrevAdjustmentMode == EC0AdjustmentMode::Additional)
-            {
-                Parent->AspectRatio += AutoAspectRatio;
-                Parent->FOV += AutoFOV;
-            }
-        }
-
-        Parent->PrevAdjustmentMode = Parent->AdjustmentMode;
     }
 
-    float FinalAspectRatio = Parent->AspectRatio;
-    float FinalFOV = Parent->FOV;
-    if (Parent->AdjustmentMode == EC0AdjustmentMode::Automatic)
+    // Find camera rotation
+    FVector Forward = (ParentGrid->GetActorLocation() - GetActorLocation()).GetSafeNormal();
+    FVector RotatedUpDir = ParentGrid->GetActorRightVector().RotateAngleAxis(-90.f, ParentGrid->GetActorUpVector());
+    FVector Right = FVector::CrossProduct(RotatedUpDir, Forward).GetSafeNormal();
+    FVector AdjustedUp = FVector::CrossProduct(Forward, Right);
+    FMatrix RotationMatrix = FMatrix(Forward, Right, AdjustedUp, FVector::ZeroVector);
+    SetActorRotation(RotationMatrix.Rotator());    
+
+    const float Margin = GridMarginInTiles * 2.f;
+    const float AutoAspectRatio = (ParentGrid->Width + Margin) / (ParentGrid->Length + Margin);
+    const float CamDist = FVector::Dist(ParentGrid->GetActorLocation(), GetActorLocation());
+    const float HalfGridWidth = (ParentGrid->Width + Margin) * ParentGrid->TileSizeInCm / 2.f;
+    const float AutoFOV = FMath::RadiansToDegrees(2 * FMath::Atan(HalfGridWidth / CamDist));
+    // Adjust FOV + aspect ratio based on mode
+    if (AdjustmentMode != PrevAdjustmentMode)
+    {
+        if (AdjustmentMode == EC0AdjustmentMode::Automatic)
+        {
+            AspectRatio = 0.f;
+            FOV = 0.f;
+        }
+        else if (AdjustmentMode == EC0AdjustmentMode::Additional &&
+            PrevAdjustmentMode == EC0AdjustmentMode::Manual)
+        {            
+            AspectRatio -= AutoAspectRatio;
+            FOV -= AutoFOV;
+        }
+        else if (AdjustmentMode == EC0AdjustmentMode::Manual)
+        {
+            if (PrevAdjustmentMode == EC0AdjustmentMode::Automatic)
+            {
+                AspectRatio = AutoAspectRatio;
+                FOV = AutoFOV;
+            }
+            else if (PrevAdjustmentMode == EC0AdjustmentMode::Additional)
+            {
+                AspectRatio += AutoAspectRatio;
+                FOV += AutoFOV;
+            }
+        }
+
+        PrevAdjustmentMode = AdjustmentMode;
+    }
+
+    float FinalAspectRatio = AspectRatio;
+    float FinalFOV = FOV;
+    if (AdjustmentMode == EC0AdjustmentMode::Automatic)
     {
         FinalAspectRatio = AutoAspectRatio;
         FinalFOV = AutoFOV;
     }
-    else if (Parent->AdjustmentMode == EC0AdjustmentMode::Additional)
+    else if (AdjustmentMode == EC0AdjustmentMode::Additional)
     {
-        FinalAspectRatio = AutoAspectRatio + Parent->AspectRatio;
-        FinalFOV = AutoFOV + Parent->FOV;
+        FinalAspectRatio = AutoAspectRatio + AspectRatio;
+        FinalFOV = AutoFOV + FOV;
     }
-    else if (Parent->AdjustmentMode == EC0AdjustmentMode::Manual)
+    else if (AdjustmentMode == EC0AdjustmentMode::Manual)
     {
-        FinalAspectRatio = Parent->AspectRatio;
-        FinalFOV = Parent->FOV;
+        FinalAspectRatio = AspectRatio;
+        FinalFOV = FOV;
+    }
+    
+    if (UCameraComponent* Camera = GetCameraComponent())
+    {
+        Camera->SetAspectRatio(FinalAspectRatio);
+        Camera->SetFieldOfView(FinalFOV);
     }
 
-    if (UCameraComponent* Component = GetCameraComponent())
+    // Update margin overlays
+    TopMarginComponent->SetVisibility(TopOverlayMargin > 0.f);
+    BottomMarginComponent->SetVisibility(BottomOverlayMargin > 0.f);
+    RightMarginComponent->SetVisibility(RightOverlayMargin > 0.f);
+    LeftMarginComponent->SetVisibility(LeftOverlayMargin > 0.f);
+    // Distance of overlay panels from cam centre
+    const float OverlayDist = 50.f;
+    const float TileSizeInUnits = ParentGrid->TileSizeInCm / 100.f;
+    for (int32 i = 0; i < 4; i++)
     {
-        Component->SetAspectRatio(FinalAspectRatio);
-        Component->SetFieldOfView(FinalFOV);
+        UStaticMeshComponent* MarginComponent = TopMarginComponent;
+        FVector Location;
+        FVector Scale;
+        if (i == 0)
+        {
+            MarginComponent = TopMarginComponent;
+            Location = ParentGrid->GetActorLocation() + ParentGrid->GetActorForwardVector() * ((ParentGrid->Length - TopOverlayMargin) / 2) * ParentGrid->TileSizeInCm;
+            Scale = FVector(TopOverlayMargin * TileSizeInUnits, ParentGrid->Width * TileSizeInUnits, 1.f);
+        }
+        if (i == 1)
+        {
+            MarginComponent = BottomMarginComponent;
+            Location = ParentGrid->GetActorLocation() - ParentGrid->GetActorForwardVector() * ((ParentGrid->Length - BottomOverlayMargin) / 2) * ParentGrid->TileSizeInCm;
+            Scale = FVector(BottomOverlayMargin * TileSizeInUnits, ParentGrid->Width * TileSizeInUnits, 1.f);
+        }
+        else if (i == 2)
+        {
+            MarginComponent = RightMarginComponent;
+            Location = ParentGrid->GetActorLocation() + ParentGrid->GetActorRightVector() * ((ParentGrid->Width - RightOverlayMargin) / 2) * ParentGrid->TileSizeInCm;
+            Scale = FVector(ParentGrid->Length * TileSizeInUnits, RightOverlayMargin * TileSizeInUnits, 1.f);
+        }
+        else if (i == 3)
+        {
+            Location = ParentGrid->GetActorLocation() - ParentGrid->GetActorRightVector() * ((ParentGrid->Width - LeftOverlayMargin) / 2) * ParentGrid->TileSizeInCm;
+            Scale = FVector(ParentGrid->Length * TileSizeInUnits, LeftOverlayMargin * TileSizeInUnits, 1.f);
+            MarginComponent = LeftMarginComponent;
+        }
+
+        const FVector VectorToCam = GetActorLocation() - Location;
+        const float DistToCam = VectorToCam.Length();
+        const float MoveRatio = (DistToCam - OverlayDist) / DistToCam;
+        Location += (VectorToCam * MoveRatio);
+        Scale *= 1.f - MoveRatio;
+        MarginComponent->SetWorldLocation(Location);        
+        MarginComponent->SetUsingAbsoluteScale(false);
+        MarginComponent->SetWorldScale3D(Scale);        
+    }
+}
+
+#pragma region Editor Functions
+
+void AC0TTRPGCam::ToggleGridVisibility()
+{
+    if (ParentGrid)
+    {
+        ParentGrid->ToggleVisibility();
+    }
+}
+
+void AC0TTRPGCam::ApplyGlobally()
+{
+    if (ParentGrid)
+    {
+        ParentGrid->ApplyGlobally();
+    }
+}
+
+#pragma endregion Editor Functions
+
+#pragma region PIP
+
+void AC0TTRPGCam::OnSelectActor(const bool bInPrevShowPIP)
+{
+    bPrevShowPIP = bInPrevShowPIP;
+    SetShowPIP(bShowPIP);
+}
+
+void AC0TTRPGCam::OnDeselectActor()
+{
+    ULevelEditorViewportSettings* ViewportSettings = GetMutableDefault<ULevelEditorViewportSettings>();
+    if (ViewportSettings)
+    {
+        SetShowPIP(bPrevShowPIP);
+    }
+}
+
+void AC0TTRPGCam::SetShowPIP(const bool bShow)
+{
+    ULevelEditorViewportSettings* ViewportSettings = GetMutableDefault<ULevelEditorViewportSettings>();
+    if (ViewportSettings)
+    {
+        ViewportSettings->bPreviewSelectedCameras = bShow;
+        ViewportSettings->SaveConfig();
+    }
+}
+
+#pragma endregion PIP
+
+void AC0TTRPGCam::HandleDebugDraw()
+{
+    if (bDebugDrawLineToFocalPoint)
+    {
+        DrawDebugLine(GetWorld(), GetActorLocation(), ParentGrid->GetActorLocation(), FColor::Red, false, 1.f);
+        FTimerHandle Handle;
+        GetWorld()->GetTimerManager().SetTimer(Handle, this, &AC0TTRPGCam::HandleDebugDraw, 1.0f, false);
     }
 }
--- Source/BeneosTools/Public/C0TTRPGCam.h
+++ Source/BeneosTools/Public/C0TTRPGCam.h
@@ -22,9 +22,9 @@ enum class EC0AdjustmentMode : uint8
     E_NUM		UMETA(Hidden)
 };
 
-class AC0CamGrid;
+class AC0Grid;
 
-UCLASS(NotPlaceable, meta = (DisplayName = "Beneos Cam"))
+UCLASS(config = BeneosTools, meta = (DisplayName = "Beneos Cam"))
 class BENEOSTOOLS_API AC0TTRPGCam : public ACameraActor
 {
     GENERATED_BODY()
@@ -35,48 +35,124 @@ public:
 
     virtual void OnConstruction(const FTransform& Transform) override;
 
-    void PostEditComponentMove(bool bFinished);
+    virtual void PostEditMove(bool bFinished) override;
 
-    virtual FString GetDefaultActorLabel() const override;
+    virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 
-    void SetParent(AC0CamGrid* InParent);
+    FString GetDefaultActorLabel() const override;
 
     // Updates the camera's aspect ratio + FOV based on grid Length and Width
     // and relative camera position.
     void UpdateCamera();
 
+#pragma region Properties
+
 protected:
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Beneos Tools")
+        TObjectPtr<AC0Grid> ParentGrid;
+
+    // Sets auto-update behaviour of cam's FOV + Aspect Ratio
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Advanced")
+        EC0AdjustmentMode AdjustmentMode;
+
+        EC0AdjustmentMode PrevAdjustmentMode;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Advanced", meta = (EditCondition = "AdjustmentMode != EC0AdjustmentMode::Automatic", ClampMin = "0.1", ClampMax = "100.0"))
+        float AspectRatio;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Advanced", meta = (EditCondition = "AdjustmentMode != EC0AdjustmentMode::Automatic", UIMin = "5.0", UIMax = "170", ClampMin = "0.001", ClampMax = "360.0", Units = deg))
+        float FOV;
+
+    // Overlay from the edge of the screen to be covered with black, in tiles
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Margin")
+        float TopOverlayMargin;
+
+    // Overlay from the edge of the screen to be covered with black, in tiles
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Margin")
+        float BottomOverlayMargin;
+
+    // Overlay from the edge of the screen to be covered with black, in tiles
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Margin")
+        float RightOverlayMargin;
+
+    // Overlay from the edge of the screen to be covered with black, in tiles
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Margin")
+        float LeftOverlayMargin;
+
     // Margin between the grid edge and the frame of the camera
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools")
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools|Advanced")
         float GridMarginInTiles;
-
-    // Length in tiles
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools", meta = (ClampMin = 0.1f))
-        float Length;
-
-    // Width in tiles
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools", meta = (ClampMin = 0.1f))
-        float Width;
-
-    // Tile size in Unreal units (cm). Default is 152.4 ~= 5ft
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools")
-        float TileSizeInCm;
 
     // Camera's up vector will always be this
     /*UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Beneos Tools")
         FVector UpDirection;*/
 
+        // Tick to draw a line between the camera and its focal point
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Beneos Tools|Advanced")
+        bool bDebugDrawLineToFocalPoint;
+
     // Toggles whether this camera shows a picture-in-picture preview 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Beneos Tools")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Beneos Tools|Advanced")
         bool bShowPIP;
+
+#pragma endregion Properties
+
+#pragma region Editor Functions
+
+    // Hides/shows the parent grid
+    UFUNCTION(CallInEditor, Category = "Beneos Tools")
+        void ToggleGridVisibility();
+
+    // Applies the visual settings of the parent grid (colour, line width, visibility) to all grids in the scene
+    UFUNCTION(CallInEditor, Category = "Beneos Tools")
+        void ApplyGlobally();
+
+#pragma endregion Editor Functions
+
+#pragma region PIP
+
+public:
+
+    // Handles PIP disabling
+    void OnSelectActor(const bool bInPrevShowPIP);
+    void OnDeselectActor();
+    bool GetPrevShowPIP() { return bPrevShowPIP; }
+
+private:
+
+    // Value of ULevelEditorViewportSettings::bPreviewSelectedCameras setting before selected
+    bool bPrevShowPIP;
+    void SetShowPIP(const bool bShow);
+
+#pragma endregion PIP
+
+#pragma region Margin
+
+    TObjectPtr<UStaticMeshComponent> TopMarginComponent;
+    TObjectPtr<UStaticMeshComponent> BottomMarginComponent;
+    TObjectPtr<UStaticMeshComponent> RightMarginComponent;
+    TObjectPtr<UStaticMeshComponent> LeftMarginComponent;
+
+#pragma endregion Margin
 
 private:
 
     void HandleDebugDraw();
 
-    FVector CameraFocusPoint;
+    bool bInitialized;
 
-    AC0CamGrid* Parent;
+    AC0Grid* PrevParentGrid;
 
 };
+
+#pragma region Utility
+
+bool FindLineIntersection(
+    const FVector2D& Point1, const FVector2D& Dir1,
+    const FVector2D& Point2, const FVector2D& Dir2,
+    FVector2D& IntersectionPoint);
+
+FVector2D ProjectPointOntoLine(const FVector2D& Point, const FVector2D& LinePoint, const FVector2D& LineDirection);
+
+#pragma endregion Utility
